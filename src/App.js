08b66ee8bbcd4be3d7d6834dac51d8e6a30d65b7
Samowusu <samuelowusu958@gmail.com>
2022-02-16 06:01:09 +0000

    unused import glossary removed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import "./App.css";
 import Landing from "./pages/landing/Landing";
 
-import Glossary from "./components/glossary/Glossary";
-
 function App() {
-  // return <Glossary />;
   return (
     <div className="App">
       <Landing />
