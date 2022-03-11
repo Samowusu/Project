55184e3a8d89f142a84d3489b1ef1d6722d76d54
Samowusu <samuelowusu958@gmail.com>
2022-03-11 05:45:48 +0000

    router styling issue solved partially

--- src/components/header/navigation/NavBar.js
+++ src/components/header/navigation/NavBar.js
@@ -5,11 +5,12 @@ import logo from "../../../assets/images/logo.png";
 import Button from "../../UI/button/Button";
 
 function NavBar() {
-  const navLinkStyle = ({ isActive }) => {
-    return {
-      textDecoration: isActive ?? "underline",
-    };
-  };
+  // const navLinkStyle = ({ isActive }) => {
+  //   console.log({ isActive });
+  //   return {
+  //     textDecoration: isActive ? "underline" : "none",
+  //   };
+  // };
 
   return (
     <div className={classes["navBar-main"]}>
@@ -17,12 +18,8 @@ function NavBar() {
         <div className={classes["navBar-left"]}>
           <img src={logo} alt="MedSearch" />
           <nav>
-            <NavLink to={"/home"} style={navLinkStyle}>
-              Find Hospital
-            </NavLink>
-            <NavLink to={"/doctors"} style={navLinkStyle}>
-              Find Doctor
-            </NavLink>
+            <NavLink to={"/home"}>Find Hospital</NavLink>
+            <NavLink to={"/doctors"}>Find Doctor</NavLink>
             {/* <NavLink to={"/professionalNetwork"}>Professional Network</NavLink>
             <NavLink to={"/FAQS"}>FAQs</NavLink>
             <NavLink to={"/strive2thrive"}> Strive2Thrive</NavLink>
